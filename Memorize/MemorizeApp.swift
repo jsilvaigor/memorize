14c7b3a0e5eebb70056a33d568fb64013a4e68eb
Igor Silva <igorjose15@gmail.com>
2020-08-09 18:49:14 -0300

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Igor Silva on 8/9/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
